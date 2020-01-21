d23f93e4268ff60844c44c78a972792e8cc5a331
kelshmo <kelsey.montgomery@sagebase.org>
2020-01-21 01:25:05 +0000

    code to repo dream 1.17.3

--- cmc-rnaseq-de/reproduce_dream.R
+++ cmc-rnaseq-de/reproduce_dream.R
@@ -1,20 +1,44 @@
-library("tibble")
+devtools::install_github("GabrielHoffman/variancePartition@3287d7b")
+library(variancePartition)
+library(tibble)
+library(synapser)
 synLogin()
 param = BiocParallel::SnowParam(4, "SOCK", progressbar=TRUE)
 BiocParallel::register(param)
 
-metadata <- readr::read_csv(synGet("syn21488871")$path) %>% 
+COVARIATES <- readr::read_csv(synGet("syn21488871")$path) %>% 
   tibble::column_to_rownames(var = "SampleID")
 
-counts <- readr::read_csv(synGet("syn21488882")$path) %>% 
-  tibble::column_to_rownames(var = "geneID") %>% 
-  as.matrix()
+NEW.COUNTS <- readRDS(synGet("syn21535143")$path)
 
-tmp = counts
+CQN.GENE_EXPRESSION <- readRDS(synGet("syn21535146")$path)
+
+# Post adjusted formula
+random_effect <- "Individual_ID"
+adjust.covars <- c("Dx.Tissue", "RIN")
+formula <- glue::glue("~ ", glue::glue_collapse(adjust.covars, sep = " + ")) %>% 
+  glue::glue(" + (1|{random_effect})")
+
+tmp = NEW.COUNTS
 tmp[is.na(tmp)] = 0
+geneExpr = DGEList(tmp)
+geneExpr = edgeR::calcNormFactors(geneExpr)
 
-dream_formula <- ~ Dx.Tissue + (1|Individual_ID)
+VOOM.GENE_EXPRESSION = variancePartition::voomWithDreamWeights(counts = geneExpr, 
+                                                               formula = formula,
+                                                               data = COVARIATES)
 
-vobjDream <- variancePartition::voomWithDreamWeights(counts = tmp, formula = dream_formula, data = metadata)
+# Fit linear model using new weights and new design
+# CQN NA counts to 0 counts for input to dream()
+tmp = CQN.GENE_EXPRESSION$E
+tmp[is.na(tmp)] = 0
+VOOM.GENE_EXPRESSION$E = tmp
+ADJUSTED.FIT = variancePartition::dream(exprObj = VOOM.GENE_EXPRESSION, 
+                                        formula = formula,
+                                        data = COVARIATES,
+                                        computeResiduals = TRUE)
+
+RESIDUAL.GENE_EXPRESSION = residuals.MArrayLM(ADJUSTED.FIT, CQN.GENE_EXPRESSION$E)
+
 
 
