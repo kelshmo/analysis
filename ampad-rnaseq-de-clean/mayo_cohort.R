ce5972012447440472b3bc06860afd9c04fb4915
kelshmo <kelsey.montgomery@sagebase.org>
2020-02-11 01:57:41 +0000

    clean schema

--- ampad-rnaseq-de-clean/mayo_cohort.R
+++ ampad-rnaseq-de-clean/mayo_cohort.R
@@ -68,22 +68,32 @@ metadata <- metadata %>%
 
 # Harmonize Tissue, AgeAtDeath, Diagnosis, sex
 metadata <- metadata %>%
-  dplyr::rename(tissue = Tissue) %>%
-  dplyr::mutate(tissue = gsub("CER", "CBE", tissue)) %>%
-  dplyr::mutate(AgeAtDeath = gsub("_or_above", "", AgeAtDeath)) %>%
-  dplyr::rename(age_death = AgeAtDeath) %>%
-  dplyr::rename(diagnosis = Diagnosis) %>%
-  dplyr::mutate(diagnosis = case_when(
-      diagnosis == "Control" ~ "control",
-      diagnosis == "Pathologic Aging" ~ "other",
-      diagnosis == "PSP" ~ "other",
-      TRUE ~ diagnosis)
-  ) %>%
-  dplyr::mutate(tissue_diagnosis = paste(tissue, diagnosis, sep = "_")) %>%
-  dplyr::rename(source = Source) %>%
-  dplyr::mutate(source_tissue_diagnosis = paste(source, tissue_diagnosis, sep = "_")) %>%
-  dplyr::rename(sex = Gender) %>%
-  dplyr::mutate(APOE4 = NA)
+  dplyr::rename(tissue = Tissue,
+                age_death = AgeAtDeath,
+                diagnosis = Diagnosis,
+                source = Source,
+                sex = Gender,
+                sampleid = ID,
+                donorid = Donor_ID,
+                pmi = PMI,
+                flowcell = Flowcell,
+                rin = RIN,
+                pct_pf_reads_aligned = `AlignmentSummaryMetrics__PCT_PF_READS_ALIGNED`,
+                pct_coding_bases = `RnaSeqMetrics__CODING_BASES`,
+                pct_intergenic_bases = `RnaSeqMetrics__PCT_INTERGENIC_BASES`,
+                pct_intronic_bases = `RnaSeqMetrics__INTRONIC_BASES`,
+                pct_ribosomal_bases = `RnaSeqMetrics__PCT_RIBOSOMAL_BASES`) %>%
+  dplyr::mutate(tissue = gsub("CER", "CBE", tissue),
+                age_death = gsub("_or_above", "", age_death),
+                diagnosis = case_when(
+                  diagnosis == "Control" ~ "control",
+                  diagnosis == "Pathologic Aging" ~ "other",
+                  diagnosis == "PSP" ~ "other",
+                  TRUE ~ diagnosis),
+                tissue_diagnosis = paste(tissue, diagnosis, sep = "_"),
+                source_tissue_diagnosis = paste(source, tissue_diagnosis, sep = "_"),
+                APOE4 = NA,
+                pmi = NA)
 
 # Add APOE4 genotype = 0,1,2
 metadata$APOE4 <- 0
@@ -95,10 +105,11 @@ metadata$tissue_APOE4 <- paste(metadata$tissue, metadata$APOE4, sep = "_")
 
 # Compute square of RIN
 metadata <- metadata %>%
-  dplyr::mutate(RIN2 = RIN^2)
+  dplyr::mutate(`rin2` = rin^2)
 
 # Predict missing PMI
-PMI <- metadata[,c("PMI", "ID", "Donor_ID")]
+metadata$pmi <- NA
+PMI <- metadata[,c("pmi", "sampleid", "donorid")]
 
 # TODO: PMI approximation
 # Get expression (convert counts to cpm)
@@ -106,8 +117,15 @@ PMI <- metadata[,c("PMI", "ID", "Donor_ID")]
 # ind = which(rowSums(expr>=1)/dim(expr)[2] >= 0.5)
 # expr = expr[ind,]
 
+# Filter for variables to be used in downstream analysis as covariates
+covariates <- c("sampleid", "donorid", "source", "diagnosis", "tissue_diagnosis", "source_tissue_diagnosis",
+                "tissue_APOE4", "APOE4", "sex", "rin", "rin2", "age_death", "pmi",
+                "pct_pf_reads_aligned", "pct_coding_bases", "pct_intergenic_bases", "pct_intronic_bases",
+                "pct_ribosomal_bases")
+
+metadata <- dplyr::select(metadata, covariates)
 
 # Remove samples from counts matrix
-counts <- counts[,colnames(counts) %in% metadata$ID]
+counts <- counts[,colnames(counts) %in% metadata$sampleid]
 
 # TODO: appropriately parse all the synIDs and version to add to provenance with config::get()
